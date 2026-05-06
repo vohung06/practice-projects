4b45bc879bba37e7863d28ac1b00d8d755fe782f
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-05-06 16:27:13 +0700

    in bảng caro

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -9,7 +9,14 @@ int main() {
 		{' ', 'O', ' '}
 	};
 	
-	for (int i = 0; i < 3; i++) {
-		cout << board[i][0] << board[i][1] << board[i][2] << endl;
-	}	
+	cout << "   |   |   " << endl;
+	cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
+	cout << "___|___|___" << endl;
+	cout << "   |   |   " << endl;
+	cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
+	cout << "___|___|___" << endl;
+	cout << "   |   |   " << endl;
+	cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+	cout << "   |   |   " << endl;
+
 }
