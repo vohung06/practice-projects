c1a5f3f0d39efca8ebbe6f44d65a254ff479b0a6
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-05-29 11:02:42 +0700

    code class GoiBasic

--- cpp/quan-li-goi-cuoc.cpp
+++ cpp/quan-li-goi-cuoc.cpp
@@ -16,6 +16,20 @@ class GoiCuoc {
 		virtual double tinhTC() = 0;
 };
 
+class GoiBasic : public GoiCuoc {
+	public:
+		GoiBasic() {
+		}
+		double tinhCDT() override {
+			return thoiGianGoi * 1000;
+		}
+		double tinhCI() override {
+			return luuLuong * 2000;
+		}
+		double tinhTC() override {
+			return (tinhCDT() + tinhCI()) * 1.1;
+		}
+};
 
 int main() {
 	return 0;
