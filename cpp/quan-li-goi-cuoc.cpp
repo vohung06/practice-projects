f4df5284e0ee1dcfcbd43dd464b2ac4a6c35d528
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-05-29 10:59:26 +0700

    code class GoiCuoc

--- cpp/quan-li-goi-cuoc.cpp
+++ cpp/quan-li-goi-cuoc.cpp
@@ -2,6 +2,21 @@
 #include <string>
 using namespace std;
 
+class GoiCuoc {
+	protected:
+		double thoiGianGoi;
+		double luuLuong;
+	public:
+		GoiCuoc() {
+			thoiGianGoi = 0;
+			luuLuong = 0;
+		}
+		virtual double tinhCDT() = 0;
+		virtual double tinhCI() = 0;
+		virtual double tinhTC() = 0;
+};
+
+
 int main() {
 	return 0;
 }
