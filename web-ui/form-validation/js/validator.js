f73edfd9a1e0f3cd0a7ae70eeaf8caabf08fc4c4
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-07-03 17:50:57 +0700

    validate on file

--- web-ui/form-validation/js/validator.js
+++ web-ui/form-validation/js/validator.js
@@ -68,8 +68,21 @@ function Validator(options) {
                     var formValues = Array.from(enableInputs).reduce(function (values, input) {
                         switch (input.type) {
                             case "radio":
-                            case "checkbox":
                                 values[input.name] = formElement.querySelector("input[name='" + input.name + "']:checked").value;
+                                break;
+                            case "checkbox":
+                                if (!input.matches(":checked")) {
+                                    values[input.name] = "";
+                                    return values;
+                                }
+                                if (!Array.isArray(values[input.name])) {
+                                    values[input.name] = [];
+                                }
+
+                                values[input.name].push(input.value);
+                                break;
+                            case "file":
+                                values[input.name] = input.files;
                                 break;
                             default:
                                 values[input.name] = input.value;
