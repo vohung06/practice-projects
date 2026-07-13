9f0118c6874553f398e82ac894c4f385cfa891e3
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-07-13 10:40:34 +0700

    finish

---
+++ dsa-leetcode/485_MaxConsecutiveOnes.cpp
@@ -0,0 +1,29 @@
+//Given a binary array nums, return the maximum number of consecutive 1's in the array.
+//
+// 
+//Example 1:
+//
+//Input: nums = [1,1,0,1,1,1]
+//Output: 3
+//Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+//Example 2:
+//
+//Input: nums = [1,0,1,1,0,1]
+//Output: 2
+
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+    int count = 0;
+    int maxCount = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == 1) {
+            count++;
+            if (maxCount < count) {
+                maxCount = count;
+            }
+        } else {
+            count = 0;
+        }    
+    }  
+    return maxCount;
+}
+ 
